10741fda1ee54eed124998a6567b9f3d4b9037eb
chunqiuyiyu <chunqiuyiyu@163.com>
2016-01-08 17:49:50 +0800

    add a tools to down html5 game resources

---
+++ downfaster.py
@@ -0,0 +1,91 @@
+#coding=utf-8
+import urllib  
+import re
+import os
+import Queue
+
+q = Queue.Queue()
+
+#we need to filter the incorrect resource path
+game_name = ''
+def deal_path(n):
+	if  n.count(game_name) > 0:
+		return n
+
+def mak_dirs(filename):
+	f = open(filename)
+	s = f.read()
+	f.close()
+	#add new reg pattern:some site use https protocol
+	#the url would noet be long,I limit it in 80 characters
+	reg=re.compile(r'https?://.{0,80}\.(?:png|js|ogg|json|mp3|jpg|css|bin|plist|wav|xml)')
+	x = re.findall(reg,s)
+	x = filter(deal_path, x)
+
+	count = 0
+	length = len(x)
+
+	while (count<length):
+		url = x[count]
+		print url
+		path = "res/"+"/".join(url.split('/')[3:-1])
+		if os.path.exists(path):
+			pass
+		else:
+			#can't create the same path in os
+			os.makedirs(path)
+		count += 1
+	return x;
+def down_game(x,root):
+	#download resource
+	count = 1
+	for res in x:
+		#down img, js
+		local = "res/"+"/".join(res.split('/')[3:])
+		urllib.urlretrieve(res, local)
+		s="资源总数："+str(len(x))+"，已下载："+str(count)
+		root.event_generate("<<TIPS>>", when="tail")
+		q.put(s)
+		count +=1
+
+def html_data(data):
+	'''
+	deal some complex situation
+	some game html will be contained in site html
+	we must deal it
+	'''
+	if(len(data)==1):
+		#if only one html,we don't deal it
+		return data[0]
+	else:
+		#if there are many htmls,we need the shortest html
+		tmp = min(map(lambda x:len(x) ,data))
+		tmp = filter(lambda x:len(x)==tmp,data)[0]
+
+		return tmp
+
+def down_html(filename):
+	'''
+	down and write html file
+	'''
+	f = open(filename)
+	s = f.read()
+	f.close()
+	#down html
+	reg=re.compile(r'<!.*>')
+	x = re.findall(reg,s)
+	x = html_data(x)
+
+	if(x.find(r'\r')!=-1):
+		x = x.split(r'\r\n')
+	else:
+		x = x.split(r'\n')
+	ft = open("res/"+game_name+".html", 'w')
+	#prettify html code
+	for entry in x :
+	    try:
+	        ft.write(entry.replace(r'\"',r'"').replace(r'\t','')+'\n')
+	    except:
+	        log.error('write file error!')
+	#close the file if we have written something in it
+	ft.close()
---
+++ window.py
@@ -0,0 +1,88 @@
+#coding=utf-8
+import Tkinter
+import ttk
+import win32ui
+import webbrowser
+import downfaster
+import threading
+
+class ToolGUI():
+    def __init__(self):
+	self.root=root= ttk.Tkinter.Tk()
+	#set window title,size and icon
+	root.wm_title('downfaster 1.0')
+
+	root.withdraw()    #hide window
+	screen_width = root.winfo_screenwidth()
+	screen_height = root.winfo_screenheight() - 100    #under windows, taskbar may lie under the screen
+	root.resizable(False,False)
+	root.iconbitmap('icon.ico')
+	#add some widgets to the root window...
+	#game name entry
+
+	ttk.Label(root, text=u'请悉知', foreground='brown').grid(row=0, sticky='E', padx=5, pady=10)
+	ttk.Label(root, text=u'这是一个简单的下载工具，你可以用它来快速下载html5\n小游戏中的资源，如有建议问题，欢迎点击关于按钮反馈:-)',foreground='brown', 
+		justify='left').grid(row=0, column=1, sticky='W', pady=10)
+	ttk.Label(root, text=u'请输入小游戏名称：').grid(row=1, column=1, sticky='W', padx=5, pady=0)
+	self.entry=ttk.Entry(root)
+	self.entry.grid(row=1, column=1, sticky='E', padx=30)
+
+	ttk.Label(root, text=u'选择har文件路径（不要有中文）：').grid(row=2, column=1, sticky='W', padx=5, pady=0)	
+
+	self.select_btn = ttk.Button(root, text=u'选择')
+	self.select_btn.bind('<Button-1>', self.select_har)
+	self.select_btn.grid(row=2,column=1,pady=10, sticky='E', padx=30)
+
+	self.tips=ttk.Tkinter.Label(root, text=u"提示：har文件可以从Chrome中下载得到")
+	self.tips.grid(row=3, column=1, sticky='W')	
+
+	self.down_btn = ttk.Button(root, text=u'下载')
+	self.down_btn.bind('<Button-1>', self.down_res)
+	self.down_btn.grid(row=4,column=1,pady=10, sticky='W')
+
+	self.about_btn = ttk.Button(root, text=u'关于')
+	self.about_btn.bind('<Button-1>', self.about_me)
+	self.about_btn.grid(row=4,column=1,pady=10, sticky='E', padx=30)
+
+	root.update_idletasks()
+	root.deiconify()    #now window size was calculated
+	root.withdraw()     #hide window again
+	root.geometry('%sx%s+%s+%s' % (root.winfo_width() + 10, root.winfo_height() + 10, (screen_width - root.winfo_width())/2, (screen_height - root.winfo_height())/2) )    #center window on desktop
+	root.deiconify()
+
+	root.bind("<<TIPS>>",lambda evt:self.update_tips(downfaster.q.get()))
+	root.mainloop()
+
+    def down_res(self, event):
+
+    	downfaster.game_name=self.entry.get()
+    	print downfaster.game_name
+    	urls=downfaster.mak_dirs(self.filename)
+    	self.create_down(urls,self.root)
+    	self.create_html(self.filename)
+
+    def select_har(self, event):
+    	# 1 for open file dialog
+    	self.dlg = win32ui.CreateFileDialog(1) 
+    	 # set the initialdir when the dialog is open
+	self.dlg.SetOFNInitialDir(r'C:\Users\win 7\Downloads')
+	self.dlg.DoModal()
+	 # get selected file path name
+	self.filename = self.dlg.GetPathName()
+	print self.filename
+	self.tmp="当前路径："+ self.filename
+	self.update_tips(self.tmp)
+
+    def about_me(self, event):
+        browser = webbrowser.get()
+        browser.open('https://github.com/chunqiuyiyu')
+
+    def update_tips(self,str):
+        self.tips['text'] = str
+        self.tips.update()
+    def create_down(self,urls,root):
+        threading.Thread(target=downfaster.down_game, args=(urls,root)).start()
+    def create_html(self,filename):
+        threading.Thread(target=downfaster.down_html, args=(filename,)).start()
+
+app = ToolGUI()
