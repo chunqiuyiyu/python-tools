c3a8040cb36afcfa7657d62eb634430b860493fe
chunqiuyiyu <chunqiuyiyu@163.com>
2016-03-28 15:26:33 +0800

    update url regular expression

--- downfaster/downfaster.py
+++ downfaster/downfaster.py
@@ -17,7 +17,7 @@ def mak_dirs(filename):
 	s = f.read()
 	f.close()
 	#add new reg pattern:some site use https protocol
-	reg=re.compile(r'https?://.*?\.(?:png|js|ogg|json|mp3|jpg|css|bin|plist|wav|xml|fnt|ini)(?!(?:\w|\\|=|\())')
+	reg=re.compile(r'https?://.[^=+\\]*?\.(?:png|js|ogg|json|mp3|jpg|css|bin|plist|wav|xml|fnt|ini)(?!(?:\w|\\|=|\(|\.))')
 	x = re.findall(reg,s)
 	x = filter(deal_path, x)
 
