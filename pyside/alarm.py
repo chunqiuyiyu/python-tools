eb529093ca9cb56433eb8870e77843feb6288a51
chunqiuyiyu <1725651735@qq.com>
2017-05-14 10:59:19 +0800

    add new project:pyside and alarm.py in folder pyside

---
+++ pyside/alarm.py
@@ -0,0 +1,35 @@
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+import time
+
+app = QApplication(sys.argv)
+
+try:
+    due = QTime.currentTime()
+    message = "Alert!"
+
+    if len(sys.argv) < 2:
+        raise ValueError
+
+    hours, minutes = sys.argv[1].split(":")
+    due = QTime(int(hours), int(minutes))
+
+    if not due.isValid():
+        raise ValueError
+
+    if len(sys.argv) > 2:
+        message = " ".join(sys.argv[2:])
+
+except ValueError:
+    message = "Usage: alarm.py HH:MM [optional message]"
+
+while  QTime.currentTime() < due:
+    time.sleep(10)
+
+label = QLabel("<font color=red size=72><b>" + message + "</b></font>")
+label.setWindowFlags(Qt.SplashScreen)
+label.show()
+
+QTimer.singleShot(10000, app.quit)
+app.exec_()
