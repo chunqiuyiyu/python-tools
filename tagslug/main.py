fec1787e915c6f3b1ac5ec56be5397ec5db667a7
春秋一语 <1725651735@qq.com>
2018-06-01 09:27:42 +0000

    Update main.py

--- tagslug/main.py
+++ tagslug/main.py
@@ -1,8 +1,12 @@
 import os
 
-tags = [x for x in os.listdir('./') if os.path.isdir(os.path.join('./',x))]
+# Set blog path here
+path = '../../my-blogs/public/tags'
+tags = [x for x in os.listdir(path) if os.path.isdir(os.path.join(path,x))]
+tmp = '  {}: {}\n'
 
+# print(tags)
 with open('./tagslug.yaml', 'w') as f:
   f.write('tag_map:\n')
   for tag in tags:
-    f.write(tag + '\n')
+    f.write(tmp.format(tag, tag.lower()))
